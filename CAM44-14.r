be2b0cb3e349d04fc84ed6da03770f7f61780102
YafeiXu <xuyafee@gmail.com>
2014-07-08 18:27:53 +0200

    78

--- CAM44-14.r
+++ CAM44-14.r
@@ -2,6 +2,8 @@
 
 
 # install package
+install.packages("copula")
+library(copula)
 install.packages("scatterplot3d")
 library(scatterplot3d)
 set.seed(12345)
@@ -12,17 +14,17 @@ theta2=2
 C3=onacopula("G", C(theta2, 3, C(theta1, c(1,2))))
 U=rCopula(1100, C3)
 x1=qnorm(U[,1])
-x2=qt(U[,2], df=2)
+x2=qt(U[,2], df=20)
 x3=qnorm(U[,3])
 d=data.frame(x1,x2,x3)
-d=d[-which(d[,1]>=3|d[,1]<=-3),]
-d=d[-which(d[,2]>=3|d[,2]<=-3),]
-d=d[-which(d[,3]>=3|d[,3]<=-3),]
+
 
 # do plot
 s3d <- scatterplot3d(d[,1],d[,2],d[,3], main="Hierarchical Archimedean Copula", pch=16, zlab="x3", ylab="x2", xlab="x1")
 s3d$points3d(d[,1],d[,2],d[,3], col="red", pch=16)
-s3d$points3d(d[,1],d[,2],seq(-3,-3,length.out=length(d[,3])), col="lightblue", pch=16)
-s3d$points3d(seq(-3,-3,length.out=length(d[,3])),d[,2],d[,3], col="lightblue", pch=16)
+s3d$points3d(d[,1],d[,2],seq(min(floor((d[,3]))),min(floor((d[,3]))),length.out=length(d[,3])), col="lightblue", pch=16)
+s3d$points3d(seq(min(floor((d[,1]))),min(floor((d[,1]))),length.out=length(d[,3])),d[,2],d[,3], col="lightblue", pch=16)
+
+#
 
 
